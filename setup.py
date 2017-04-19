c9ffffefa44bc9a67dbf39089acc93b14c1d761c
Fan Yang <fan.yang.turner@live.co.uk>
2017-04-19 16:53:18 +0100

    Add setup file and script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pythonlearn',
+    version = '0.1',
+    description = "python learning",
+    author = 'Fan',
+    author_email = 'fan.yang@live.co.uk',
+    packages=['src.reader']
+)
